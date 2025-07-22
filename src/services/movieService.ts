e6c04ba2b81194a8b4a66d372f00edec7be53907
IrynaRK <105289016+IrynaRK@users.noreply.github.com>
2025-07-22 22:08:50 +0200

    +

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import type { MovieApiResponse } from "../types/movie";
 
-
-
-
 export interface MovieQueryParams {
     query: string;
     page: number;
@@ -15,7 +12,6 @@ const apiClient = axios.create({
     Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
     accept: 'application/json',
   },
-
 });
 
 export const fetchMovies = async ({
@@ -36,7 +32,6 @@ export const fetchMovies = async ({
     },
   });
 
-console.log('API KEY from env:', import.meta.env.VITE_TMDB_API_KEY);
   return response.data;
 };
 
